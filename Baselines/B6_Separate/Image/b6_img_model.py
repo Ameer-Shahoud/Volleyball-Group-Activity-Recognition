f5c85a546b5af4b66a7cc81b3d3543f6779dcb84
Ameer-Shahoud <ameer.shahoud.1997@gmail.com>
2025-06-26 22:09:25 +0300

    b6 ready

--- Baselines/B6_Separate/Image/b6_img_model.py
+++ Baselines/B6_Separate/Image/b6_img_model.py
@@ -1,22 +1,29 @@
 import torch
-from Baselines.B5_separate.Player.b5_player_model import B5PlayerModel
+from Baselines.B3_separate.Player.b3_player_model import B3PlayerModel
 from Enums.classification_level import ClassificationLevel
 from Abstracts.base_model import _BaseModel
-from Modules.crops_classifier_head import CropsClassifierHead
+from Modules.classifier_head import ClassifierHead
+from Modules.custom_max_pool import CustomMaxPool
+from Modules.lstm_head import LSTMHead
+from torch import nn
 
 
-class B5ImgModel(_BaseModel):
+class B6ImgModel(_BaseModel):
     def __init__(self, pretrained_player_model_path: str):
         super().__init__()
 
-        self.pretrained_player_model: B5PlayerModel = _BaseModel.load_model(
+        self.pretrained_player_model: B3PlayerModel = _BaseModel.load_model(
             model_path=pretrained_player_model_path,
         )
 
         for p in self.pretrained_player_model.parameters():
             p.requires_grad = False
 
-        self.img_head = CropsClassifierHead(
+        self.pool = CustomMaxPool(dim=1)
+
+        self.lstm = nn.LSTM(2048, 512, batch_first=True)
+
+        self.classifier = ClassifierHead(
             input_dim=512,
             hidden_dim=256,
             num_classes=len(self.get_cf().dataset.get_categories(
@@ -34,11 +41,15 @@ class B5ImgModel(_BaseModel):
 
         player_features = self.pretrained_player_model(
             x_view, return_features=True
+        ).view(
+            batch_size, players_count, frames_count, -1
         )
 
-        player_features = player_features[
-            :, -1, :].view(batch_size, players_count, -1)
-        img_outputs = self.img_head(player_features)
+        pooled_features = self.pool(player_features)
+
+        lstm_features, _ = self.lstm(pooled_features)
+
+        img_outputs = self.classifier(lstm_features[:, -1, :])
 
         return img_outputs
 
