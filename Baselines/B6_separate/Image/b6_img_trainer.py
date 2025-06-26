119a611b9d78faf97c5e4be484432a5d9ce2e3a5
Ameer-Shahoud <ameer.shahoud.1997@gmail.com>
2025-06-26 22:41:44 +0300

    fix folder name mistake

---
+++ Baselines/B6_separate/Image/b6_img_trainer.py
@@ -0,0 +1,23 @@
+from typing import Type
+from Baselines.B6_separate.Image.b6_img_model import B6ImgModel
+from Enums.classification_level import ClassificationLevel
+from Models.image_players_dataset import ImagePlayersDataset
+from Models.single_loss_trainer import SingleLossTrainer
+
+
+class B6ImgTrainer(SingleLossTrainer):
+    def __init__(self, pretrained_player_model_path: str, checkpoint_path: str = None, history_path: str = None):
+        self.pretrained_player_model_path = pretrained_player_model_path
+
+        super().__init__(
+            checkpoint_path,
+            history_path,
+            suffix=ClassificationLevel.IMAGE.value,
+            loss_levels=[ClassificationLevel.IMAGE.value]
+        )
+
+    def _get_dataset_type(self) -> Type[ImagePlayersDataset]:
+        return ImagePlayersDataset
+
+    def _get_model(self) -> B6ImgModel:
+        return B6ImgModel(self.pretrained_player_model_path)
