2a1b4eb41d627ca129d263ad1c436a442e54a72d
Ameer-Shahoud <ameer.shahoud.1997@gmail.com>
2025-06-28 22:56:29 +0300

    increasing b5 player temporal features size

--- Baselines/B5_separate/Player/b5_player_model.py
+++ Baselines/B5_separate/Player/b5_player_model.py
@@ -3,23 +3,22 @@ import torch
 from Enums.classification_level import ClassificationLevel
 from Abstracts.base_model import _BaseModel
 from Modules.backbone import BackboneModel
-from Modules.lstm_head import LSTMHead
+from torch import nn
 
 
 class B5PlayerModel(_BaseModel):
     def __init__(self):
         super().__init__()
         self.player_model = BackboneModel(level=ClassificationLevel.PLAYER)
-        # .set_backbone_requires_grad(False) \
-        # .set_backbone_layer_requires_grad('layer4', True) \
-        # .set_backbone_layer_requires_grad('fc', True)
 
-        self.lstm_head = LSTMHead(
-            num_classes=len(self.get_cf().dataset.get_categories(
-                ClassificationLevel.PLAYER)
-            )
+        self.lstm = nn.LSTM(2048, 1024, batch_first=True)
+        self.classifier = nn.Linear(
+            1024,
+            len(self.get_cf().dataset.get_categories(ClassificationLevel.PLAYER))
         )
 
     def forward(self, x: torch.Tensor, return_features=False):
         _, features = self.player_model(x)
-        return self.lstm_head(features)[1 if return_features else 0]
+        lstm_out, _ = self.lstm(features)
+        outputs = self.classifier(lstm_out[:, -1, :])
+        return lstm_out if return_features else outputs
